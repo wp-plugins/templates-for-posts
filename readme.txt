ae22606a980fb0191cd72aedf5eda696b8cf5135
nbachiyski <nbachiyski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-01 18:53:18 +0000

    Initial import
    git-svn-id: https://plugins.svn.wordpress.org/templates-for-posts/trunk@91152 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Templates for Posts ===
+Contributors: nbachiyski
+Tags: template, post, templates, posts
+Requires at least: 2.7
+Tested up to: 2.7.1
+Stable tag: 1.0
+
+The same as page templates, but for posts.
+
+== Description ==
+
+In WordPress you can set individual templates for pages, which allows you to easily include additional functionality. Unfortunately this feature is not available for posts. Here is where this plugin comes.
+
+After installtion you will be able to choose a template for each post the same way you do with pages.
+
+You can find [how to your own templates](http://codex.wordpress.org/Pages#Creating_Your_Own_Page_Templates) in the Codex.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `templates-for-posts.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the `Plugins` menu in WordPress
---
+++ templates-for-posts.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: Templates for Posts
+Plugin URI: http://wordpress.org/extend/plugins/templates-for-posts/
+Description: The same as page templates, but for posts.
+Author: Nikolay Bachiyski
+Version: 1.0
+Author URI: http://nikolay.bg/
+*/
+
+class PostTemplates {
+	function template_redirect() {
+		if (is_single()) {
+			$page_template = get_page_template();
+			if ($page_template && !preg_match('/page\.php$/', $page_template)) {
+				include($page_template);
+				die();
+			}
+		}
+	}
+
+	function save_post($post_id, $post) {
+		$page_template = isset($_POST['post_template'])? $_POST['post_template'] : '';
+		if ( !empty($page_template)  ) {
+			$page_templates = get_page_templates();
+			$post->page_template = $page_template;
+			if ( 'default' != $page_template && !in_array($page_template, $page_templates) ) {
+				return;
+			}
+			update_post_meta($post_id, '_wp_page_template',  $page_template);
+		}
+	}
+
+	function meta_box_contents($post) {
+		$page_template = get_post_meta( $post->ID, '_wp_page_template', true );
+?>
+	<label class="hidden" for="post_template"><?php _e('Post Template', 'templates-for-posts') ?></label>
+	<select name="post_template" id="post_template">
+		<option value='default'><?php _e('Default Template', 'templates-for-posts'); ?></option>
+		<?php page_template_dropdown($page_template); ?>
+	</select>
+<?php
+	}
+
+	function add_dropdown() {
+		if ( 0 != count( get_page_templates() ) ) {
+		    add_meta_box('posttemplatediv', __('Post Template', 'templates-for-posts'), array(&$this, 'meta_box_contents'), 'post');
+		}
+	}
+};
+
+$__post_templates = & new PostTemplates;
+add_action('template_redirect', array(&$__post_templates, 'template_redirect'));
+add_action('save_post', array(&$__post_templates, 'save_post'), 10, 2);
+if (is_admin()) {
+	add_action('admin_menu', array(&$__post_templates, 'add_dropdown'));
+}
